0b0d63daf790f6635433c70f74c5607d7eb980b3
Vince Lu <vincentlu@hotmail.ca>
2026-04-29 23:09:56 -0400

    April 29 2026: improved Screen spacing

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,18 +1,23 @@
+import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Screen({ children }) {
+export default function Screen({ children, contentStyle }) {
   return (
-    <SafeAreaView
-      edges={["top", "right", "left"]}
-      style={{
-        flex: 1,
-        justifyContent: "flex-start",
-        alignItems: "stretch",
-        padding: 16,
-        backgroundColor: "#f9fafb",
-      }}
-    >
-      {children}
+    <SafeAreaView edges={["top", "left", "right"]} style={styles.safeArea}>
+      <View style={[styles.content, contentStyle]}>{children}</View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#f9fafb",
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    paddingTop: 16,
+    paddingHorizontal: 16,
+  },
+});
